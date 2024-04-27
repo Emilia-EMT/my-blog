26353f2ec84f1a303fdc2de145c0fbaaf244033e
Emilia <mansong800@gmail.com>
2024-04-28 00:09:32 +0800

    Update About page

--- app.config.ts
+++ app.config.ts
@@ -2,15 +2,15 @@
 export default defineAppConfig({
   alpine: {
     title: 'Alpine',
-    description: 'The minimalist blog theme',
+    description: '',
     image: {
       src: '/social-card-preview.png',
-      alt: 'An image showcasing my project.',
+      alt: '',
       width: 400,
       height: 300
     },
     header: {
-      position: 'left', // possible value are : | 'left' | 'center' | 'right'
+      position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
@@ -19,10 +19,10 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
+        enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
-      navigation: true, // possible value are : true | false
+      navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
