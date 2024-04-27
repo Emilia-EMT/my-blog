61c0fe92596c7b600d051e45a887b0e7eb536a97
Emilia <mansong800@gmail.com>
2024-04-27 11:59:27 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,34 +14,5 @@ export default defineNuxtConfig({
     '@nuxtjs/plausible',
     // https://github.com/nuxt/devtools
     '@nuxt/devtools',
-
-    '@nuxtjs/mdc'
-  ],
-  mdc: {
-    remarkPlugins: {
-      plugins: {
-        // Register/Configure remark plugin to extend the parser
-      }
-    },
-    rehypePlugins: {
-      options: {
-        // Configure rehype options to extend the parser
-      },
-      plugins: {
-        // Register/Configure rehype plugin to extend the parser
-      }
-    },
-    headings: {
-      anchorLinks: {
-        // Enable/Disable heading anchor links. { h1: true, h2: false }
-      }
-    },
-    highlight: false, // Control syntax highlighting
-    components: {
-      prose: false, // Add predefined map to render Prose Components instead of HTML tags, like p, ul, code
-      map: {
-        //p: 'MyCustomPComponent'// This map will be used in `<MDCRenderer>` to control rendered components
-      }
-    }
-  }
+  ]
 })
