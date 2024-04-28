fdbc7ef6c98aea5ffd54b422a3909b1b25121f71
Emilia <mansong800@gmail.com>
2024-04-28 23:02:06 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 // https://github.com/nuxt-themes/alpine/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Emilia',
     description: '',
     image: {
       src: '/social-card-preview.png',
@@ -14,15 +14,15 @@ export default defineAppConfig({
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        alt: 'About' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: false, // possible value are : true | false
+        enabled: true, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
-      navigation: false, // possible value are : true | false
+      navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
@@ -39,8 +39,5 @@ export default defineAppConfig({
         href: 'https://github.com/Emilia-EMT'
       }
     },
-    form: {
-      successMessage: 'Message sent. Thank you!'
-    }
   }
 })
